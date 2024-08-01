8abe8236ecdfc865ac3e0df0a4f7fab9670cbf6d
sergey <st@ttsgroup.su>
2024-08-01 18:24:00 +0300

    finish

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import YouTubeEmbed from "./component/youtube";
+
+function App() {
+  return (
+    <div className="App">
+      <YouTubeEmbed/>
+    </div>
+  );
+}
+
+export default App;
