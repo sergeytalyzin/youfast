7c70f3f2651dcf8858f2fa9278aeb0aab841d8c2
sergey <st@ttsgroup.su>
2024-08-09 20:23:19 +0300

    fix

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,7 +1,7 @@
 export const extractYouTubeId = (urlOrId) => {
     let id = urlOrId;
 
-    // Если это полный URL, извлекаем ID из URL
+    // eslint-disable-next-line
     const urlPattern = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
     const match = id.match(urlPattern);
 
