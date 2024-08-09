6428a36f9b67cea76e1f4b571cf69c1f30c26bfd
sergey <st@ttsgroup.su>
2024-08-09 21:29:00 +0300

    Fix babel plugin dependency issue

--- src/component/cards/cards.js
+++ src/component/cards/cards.js
@@ -1,52 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import styles from "./styles.module.less";
 
-const fetchVideoTitle = async (videoId) => {
-    try {
-        const response = await fetch(`https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=${videoId}&format=json`);
-        if (response.ok) {
-            const data = await response.json();
-            return data.title;
-        } else {
-            console.error('Ошибка при получении данных о видео');
-            return 'Не удалось загрузить название';
-        }
-    } catch (error) {
-        console.error('Ошибка при получении названия видео:', error);
-        return 'Ошибка загрузки названия';
-    }
-};
-
 const Cards = ({ ids, handler }) => {
-    const [titles, setTitles] = useState({});
-
-    useEffect(() => {
-        const fetchTitles = async () => {
-            const titlesMap = {};
-            for (const id of ids) {
-                const title = await fetchVideoTitle(id);
-                titlesMap[id] = title;
-            }
-            setTitles(titlesMap);
-        };
-
-        fetchTitles();
-    }, [ids]);
+    const handleCardClick = (id) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Прокрутка вверх с плавной анимацией
+        handler(id);
+    };
 
     if (!ids?.length) return null;
 
     return (
         <div className={styles.list}>
             {ids.slice(0,100).map((id, index) => (
-                <div key={index} onClick={() => handler(id)} className={styles.card}>
+                <div key={index} onClick={() => handleCardClick(id)} className={styles.card}>
                     <img
                         src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
                         alt={`Thumbnail for ${id}`}
                         className={styles.thumbnail}
                     />
-                    <div className={styles.cardContent}>
-                        <h3>{titles[id] || `Видео ${index + 1}`}</h3>
-                    </div>
                 </div>
             ))}
         </div>
--- src/component/youtube/youtube.js
+++ src/component/youtube/youtube.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './styles.module.less'
-import { Button, Image, Input, Popover } from 'antd';
+import {Button, Input, Popover} from 'antd';
 import 'antd/dist/reset.css';
-import { useValidate } from '../../shared/useValidate';
-import { getIds, saveIds } from '../../firebase/firestoreService';
+import {useValidate} from '../../shared/useValidate';
+import {getIds, saveIds} from '../../firebase/firestoreService';
 import {extractYouTubeId} from "../../shared/utils";
 import Cards from "../cards/cards"; // Импортируем функции для работы с Firestore
 import Player from "../player/player"; // Импортируем react-youtube
@@ -12,29 +12,20 @@ const instructions = (
     <div>
         <ol>
             <li>
-                <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">Откройте YouTube</a>
+                <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">Перейдите на сайт YouTube и скопируйте ссылку на нужное видео.</a>
             </li>
-            <Image
-                width={200}
-                src="/screen.png"
-                alt="Инструкция"
-                className='image'
-            />
             <li>
-                <p>Введите ID видео YouTube в поле ввода. Пример ID видео: <code>dQw4w9WgXcQ</code></p>
+                Вставьте скопированную ссылку в поле ввода на нашем сайте.
             </li>
-            <Image
-                width={200}
-                src="/screen2.png"
-                alt="Инструкция"
-                className='image'
-            />
-            <li><h2>Наслаждаемся просмотром!</h2></li>
+            <li>
+                Нажмите кнопку "Найти".
+            </li>
         </ol>
     </div>
 );
 
 const YouTubeEmbed = () => {
+    const [inputId, setInputId] = useState('');
     const [videoId, setVideoId] = useState(''); // Введенный ID видео
     const [confirmedVideoId, setConfirmedVideoId] = useState(''); // Подтвержденный ID видео для поиска
     const [ids, setIds] = useState([]); // Массив сохраненных ID
@@ -42,16 +33,15 @@ const YouTubeEmbed = () => {
 
     // Получаем список ID при загрузке компонента
     useEffect(() => {
-        const fetchIds = async () => {
+        (async () => {
             const storedIds = await getIds();
-            const reverseIds  = storedIds.length ? storedIds.reverse() : []
-            setIds(reverseIds);
-            storedIds.length && setConfirmedVideoId(reverseIds[0])
-        };
-        fetchIds();
+            setIds(storedIds);
+            storedIds.length && setConfirmedVideoId(storedIds[0])
+        })()
     }, []);
 
     const handleInputChange = (e) => {
+        setInputId(e?.target?.value)
         const value = extractYouTubeId(e?.target?.value)
         setVideoId(value);
     };
@@ -62,9 +52,9 @@ const YouTubeEmbed = () => {
               const valid =   await checkVideoId(videoId);
                 if (valid) {
                     setConfirmedVideoId(videoId);
-                    const updatedIds = Array.from(new Set([...ids, videoId]));
+                    const updatedIds = Array.from(new Set([videoId, ...ids]));
                     setIds(updatedIds);
-                    await saveIds(updatedIds); // Сохраняем обновленный массив ID в Firestore
+                    await saveIds(updatedIds);
                 }
             } catch (error) {
                 console.error('Ошибка при проверке видео ID:', error);
@@ -73,32 +63,39 @@ const YouTubeEmbed = () => {
         } else {
             setIsValid(true);
         }
+        setInputId('')
     };
     const handlerCards = (id) => {
         setConfirmedVideoId(id)
     }
     return (
-        <div style={{ padding: '20px' }} className="body">
-            <header className="header">
-                <h1>Здесь может быть ваша реклама</h1>
-            </header>
-            <div className={styles.actions}>
-                <Popover content={instructions} title="Инструкция" trigger="click">
-                    <Button type="primary" danger className="button">Посмотреть инструкцию</Button>
-                </Popover>
-                <Input
-                    placeholder="Введите ID видео YouTube в поле ввода"
-                    value={videoId}
-                    onChange={handleInputChange}
-                    style={{ marginBottom: '10px' }}
-                />
-                <Button type='primary' onClick={handleSearchClick}>Найти</Button>
-            </div>
+        <div className={styles.wrapper}>
+            <div className="body">
+                <header className="header">
+                    <div className={styles.header}></div>
+                </header>
+                <div className={styles.container}>
+                    <div className={styles.actions}>
+                        <Popover content={instructions} title="Инструкция" trigger="click">
+                            <Button type="primary" danger className="button">Посмотреть инструкцию</Button>
+                        </Popover>
+                        <div className={styles.actions_wrapper}>
+                            <Input
+                                placeholder="Ссылка с youtube"
+                                value={inputId}
+                                onChange={handleInputChange}
+                                style={{marginBottom: '10px'}}
+                            />
+                            <Button type='primary' onClick={handleSearchClick}>Найти</Button>
+                        </div>
+                    </div>
+                </div>
 
-            {confirmedVideoId && (
-                <Player id={confirmedVideoId}/>
-            )}
-            {ids?.length && <Cards ids={ids} handler={handlerCards}/>}
+                {confirmedVideoId && (
+                    <Player id={confirmedVideoId}/>
+                )}
+                {ids?.length && <Cards ids={ids} handler={handlerCards}/>}
+            </div>
         </div>
     );
 };
